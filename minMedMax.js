e777df81dacd75b5d586d94a8ad99de74af77944
TK-Neng <88127892+TK-Neng@users.noreply.github.com>
2022-09-07 15:28:23 +0700

    Update minMedMax.js

--- minMedMax.js
+++ minMedMax.js
@@ -2,5 +2,26 @@ const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
   //code here
+  if (n1 > n2) { 
+    let temp = n1; 
+    n1 = n2; 
+    n2 = temp; 
+  }
+  if (n1 > n3) { 
+    let temp = n1; 
+    n1 = n3; 
+    n3 = temp;
+  }
+  if (n2 > n3) {  
+    let temp = n2; 
+    n2 = n3;
+    n3 = temp; 
+  }
+  let obj = { 
+    min: n1,
+    mid: n2,
+    max: n3
+  }
+  return obj;
 }
 module.exports = minMedMax
